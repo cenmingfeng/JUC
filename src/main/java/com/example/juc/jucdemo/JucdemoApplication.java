4ade1bcf249120ca372880580bb349e1e981e003
roubin.cen <bestfo@163.com>
2020-01-13 19:24:21 +0800

    test

---
+++ src/main/java/com/example/juc/jucdemo/JucdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.juc.jucdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JucdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JucdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/juc/jucdemo/JucdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.juc.jucdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JucdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
